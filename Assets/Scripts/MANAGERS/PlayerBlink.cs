3e4b9dd1db10733544c74ce29ca1697cca244710
SantiDonatoAH <santivdonato@gmail.com>
2024-07-30 08:41:01 -0300

    playerblink

---
+++ Assets/Scripts/MANAGERS/PlayerBlink.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBlink : MonoBehaviour
+{
+    SpriteRenderer renderer;
+    Color damageColor = Color.red;
+    Color normalColor;
+
+    private void Awake()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+    void Start()
+    {
+        normalColor = renderer.color;
+    }
+
+    // Update is called once per frame
+    public void Blink()
+    {
+        Invoke("EnableBlink", 0);
+        Invoke("DisableBlink", 0.2f);
+    }
+
+    void EnableBlink()
+    {
+        renderer.color = damageColor;
+    }
+
+    void DisableBlink()
+    {
+        renderer.color = normalColor;
+    }
+}
