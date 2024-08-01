d85e070e42b0937df06cd63dd5ff1a2d3b96127e
Lautaro Urso <lautarourso@gmail.com>
2024-08-01 09:22:58 -0300

    cambio de escena

--- Assets/Scripts/MANAGERS/PlayerBlink.cs
+++ Assets/Scripts/MANAGERS/PlayerBlink.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerBlink : MonoBehaviour
 {
@@ -28,7 +29,7 @@ public class PlayerBlink : MonoBehaviour
         }
         else
         {
-            Debug.Log("poder");
+            SceneManager.LoadScene("GAMEOVER");
         }
     }
 
