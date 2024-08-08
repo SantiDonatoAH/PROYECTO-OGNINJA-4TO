224ca2ac3de4a1c05091e2b376b281928cb0f413
bautistam1 <147621284+bautistam1@users.noreply.github.com>
2024-08-08 20:44:31 -0300

    audio

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("----------Audio Source----------")]
+    [SerializeField] AudioSource musicSource; 
+    [Header("----------Audio Clip----------")]
+    public AudioClip bakground; 
+
+    private static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);  // No destruir al cargar una nueva escena
+        }
+        else
+        {
+            Destroy(gameObject);  // Destruir esta instancia si ya existe otra
+            return;  // Salir del Awake para evitar ejecutar el código en la instancia destruida
+        }
+    }
+
+    private void Start()
+    {
+        musicSource.clip = bakground;
+        musicSource.Play();
+    }
+}
