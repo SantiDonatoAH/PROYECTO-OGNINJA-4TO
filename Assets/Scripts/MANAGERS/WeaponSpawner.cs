db425e0c86d21f883fd7b1a6a8d670b8d282182b
Lautaro Urso <lautarourso@gmail.com>
2024-08-26 19:18:50 -0300

    jiji

--- Assets/Scripts/MANAGERS/WeaponSpawner.cs
+++ Assets/Scripts/MANAGERS/WeaponSpawner.cs
@@ -10,24 +10,43 @@ public class WeaponSpawner : MonoBehaviour
 
     void Start()
     {
-        SpawnWeapon();
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        GameObject armaSeleccionada = armas[Random.Range(0, armas.Length)];
+        GameObject objetoExistente = GameObject.Find(armaSeleccionada.name + "(Clone)");
+
+        Instantiate(armaSeleccionada, spawnPoint.position, spawnPoint.rotation);
     }
 
     private void Update()
     {
-        timer -= Time.deltaTime;  // Restar el tiempo pasado desde el último frame
+        timer -= 0.01f;
         if (timer <= 0)
         {
-            SpawnWeapon();        // Spawnear un arma cuando el tiempo llegue a 0
-            timer = 10f;          // Reiniciar el temporizador
+            SpawnWeapon();
+            timer = 10;
         }
     }
-
     void SpawnWeapon()
     {
-        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];  // Seleccionar un punto de spawn aleatorio
-        GameObject armaSeleccionada = armas[Random.Range(0, armas.Length)];      // Seleccionar un arma aleatoria
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        GameObject armaSeleccionada = armas[Random.Range(0, armas.Length)];
+        GameObject objetoExistente = GameObject.Find(armaSeleccionada.name + "(Clone)");
 
-        Instantiate(armaSeleccionada, spawnPoint.position, spawnPoint.rotation);  // Crear una nueva instancia del arma en el punto de spawn
+        if (objetoExistente != null)
+        {
+            // Instanciar el arma en el punto de spawn
+            GameObject nuevaArma = Instantiate(armaSeleccionada, new Vector3(Random.Range(-6, 6), spawnPoint.position.y, 0), spawnPoint.rotation);
+
+            // Obtener el script del objeto clonado y deshabilitarlo
+            var tipoDelScript = System.Type.GetType(armaSeleccionada.name);
+            var script = nuevaArma.GetComponent(tipoDelScript) as MonoBehaviour;    
+            script.enabled = false;
+            
+        }
+        else
+        {
+            // Si no existe un clon, simplemente instanciar el arma
+            Instantiate(armaSeleccionada, new Vector3(Random.Range(-6, 6), spawnPoint.position.y, 0), spawnPoint.rotation);
+        }
     }
 }
