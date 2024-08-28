ca82eeba46cf246312283f53152ca7c3e9789c18
bautistam1 <147621284+bautistam1@users.noreply.github.com>
2024-08-28 15:47:33 -0300

    Camera Script (URso)

--- Assets/cameraScript.cs
+++ Assets/cameraScript.cs
@@ -10,7 +10,9 @@ public class cameraScript : MonoBehaviour
     private Rigidbody2D Ninja1rb;
     private Rigidbody2D Ninja2rb;
 
-    public int offset = 7;
+    public int maxoffset = 7;
+    public int minoffset = -7;
+    public int offsety = -5;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,9 +23,27 @@ public class cameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Ninja1rb.position.y < -offset || Ninja1rb.position.x > offset || Ninja1rb.position.x < -offset)
+        if (Ninja1rb.position.y < offsety)
         {
-            transform.position = new Vector3(transform.position.x, -5, transform.position.z);
+            transform.position = new Vector3(transform.position.x, Ninja1rb.position.y, transform.position.z);
+        }
+        else if (Ninja1rb.position.x > maxoffset || Ninja1rb.position.x < minoffset)
+        {
+            transform.position = new Vector3(Ninja1rb.position.x, transform.position.y, transform.position.z);
+        }
+
+        else if (Ninja2rb.position.y < offsety)
+        {
+            transform.position = new Vector3(transform.position.x, Ninja2rb.position.y, transform.position.z);
+        }
+        else if (Ninja2rb.position.x > maxoffset || Ninja2rb.position.x < minoffset)
+        {
+            transform.position = new Vector3(Ninja2rb.position.x, transform.position.y, transform.position.z);
+        }
+
+        else
+        {
+            transform.position = new Vector3(0, 0, 10);
         }
     }
 }
