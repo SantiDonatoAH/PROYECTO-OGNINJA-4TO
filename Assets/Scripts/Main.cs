b5fd0bb55b39ce1d41af79588921158b625c5f6f
bautistam1 <147621284+bautistam1@users.noreply.github.com>
2024-08-30 15:56:36 -0300

    mas cosasss

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class Main : MonoBehaviour
 {
     public GameObject secondPanel;
+    public GameObject PanelSettings;
+    public GameObject Panel2;
+    public GameObject PanelGame;
 
     // Start is called before the first frame update
     void Start()
     {
         secondPanel.SetActive(false);
+        PanelSettings.SetActive(false);
+        PanelGame.SetActive(false);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnclickStart()
@@ -24,4 +29,29 @@ public class Main : MonoBehaviour
         secondPanel.SetActive(true);
     }
 
+    public void OnClickSettings()
+    {
+        PanelSettings.SetActive(true);
+        secondPanel.SetActive(false);
+        PanelGame.SetActive(true);
+
+    }
+
+    public void OnBack()
+    {
+        PanelSettings.SetActive(false);
+        Panel2.SetActive(false);
+        secondPanel.SetActive(true);
+    }
+
+    public void OnButton2()
+    {
+        Panel2.SetActive(true);
+        PanelGame.SetActive(false);
+    }
+    public void OnButtonGame()
+    {
+        Panel2.SetActive(false);
+        PanelGame.SetActive(true);
+    }
 }
