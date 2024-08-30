b0655fe165660af792813c66181db79b7f4e4dfb
SantiDonatoAH <santivdonato@gmail.com>
2024-08-30 13:27:34 -0300

    etesech

---
+++ Assets/goback.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class goback : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            OnclickGoBack();
+        }
+        Time.timeScale = 1;
+    }
+
+    public void OnclickGoBack()
+    {
+        SceneManager.LoadScene("INICIO");
+    }
+}
