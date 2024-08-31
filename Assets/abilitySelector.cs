1f05fe16b800d241aedc32b338e6b0a1157ec777
bautistam1 <147621284+bautistam1@users.noreply.github.com>
2024-08-31 19:10:38 -0300

    skibidi

--- Assets/abilitySelector.cs
+++ Assets/abilitySelector.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class abilitySelector : MonoBehaviour
@@ -18,6 +19,10 @@ public class abilitySelector : MonoBehaviour
 
     public string h1;
     public string h2;
+
+    public Image image1;
+    public Image image2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,64 +37,76 @@ public class abilitySelector : MonoBehaviour
     {
         
     }
-    void Ninja1()
+   void Ninja1()
+{
+    string habilidad = abilities[Random.Range(0, abilities.Length)];
+    Debug.Log(habilidad);
+
+    if (habilidad == "salto")
     {
-        string habilidad = abilities[Random.Range(0, abilities.Length)];
-        Debug.Log(habilidad);
-
-        if (habilidad == "salto")
-        {
-            ninjacontroller.jumpForce = 10;
-        }
-
-        if (habilidad == "vida")
-        {
-            playerblink.health = 15;
-        }
-
-        if (habilidad == "daño")
-        {
-            playerblink2.restar = .75f;
-        }
-
-        if (habilidad == "velocidad")
-        {
-            ninjacontroller.moveSpeed = 6.5f;
-        }
-        if(habilidad == "cooldown")
-        {
-            bombucha.cooldownTime = .5f;
-        }
-
+        image1.sprite = Resources.Load<Sprite>("Salto");
+        ninjacontroller.jumpForce = 10;
     }
 
-        void Ninja2() 
+    if (habilidad == "vida")
     {
-        string habilidad = abilities[Random.Range(0, abilities.Length)];
-        Debug.Log(habilidad);
-
-        if (habilidad == "salto")
-        {
-            ninjacontroller2.jumpForce = 10;
-        }
-
-        if (habilidad == "vida")
-        {
-            playerblink2.health = 15;
-        }
-
-        if (habilidad == "daño")
-        {
-            playerblink.restar = .75f;
-        }
-
-        if (habilidad == "velocidad")
-        {
-            ninjacontroller2.moveSpeed = 6.5f;
-        }
-        if (habilidad == "cooldown")
-        {
-            bombucha.cooldownTime2 = .5f;
-        }
+        image1.sprite = Resources.Load<Sprite>("Vida"); // Carga la imagen correspondiente a "vida"
+        playerblink.health = 15;
     }
+
+    if (habilidad == "daño")
+    {
+        image1.sprite = Resources.Load<Sprite>("Daño"); // Carga la imagen correspondiente a "daño"
+        playerblink2.restar = .75f;
+    }
+
+    if (habilidad == "velocidad")
+    {
+        image1.sprite = Resources.Load<Sprite>("Velocidad"); // Carga la imagen correspondiente a "velocidad"
+        ninjacontroller.moveSpeed = 6.5f;
+    }
+    
+    if (habilidad == "cooldown")
+    {
+        image1.sprite = Resources.Load<Sprite>("Cooldown"); // Carga la imagen correspondiente a "cooldown"
+        bombucha.cooldownTime = .5f;
+    }
+}
+
+void Ninja2()
+{
+    string habilidad = abilities[Random.Range(0, abilities.Length)];
+    Debug.Log(habilidad);
+
+    if (habilidad == "salto")
+    {
+        image2.sprite = Resources.Load<Sprite>("Salto");
+        ninjacontroller2.jumpForce = 10;
+    }
+
+    if (habilidad == "vida")
+    {
+        image2.sprite = Resources.Load<Sprite>("Vida"); // Carga la imagen correspondiente a "vida"
+        playerblink2.health = 15;
+    }
+
+    if (habilidad == "daño")
+    {
+        image2.sprite = Resources.Load<Sprite>("Daño"); // Carga la imagen correspondiente a "daño"
+        playerblink.restar = .75f;
+    }
+
+    if (habilidad == "velocidad")
+    {
+        image2.sprite = Resources.Load<Sprite>("Velocidad"); // Carga la imagen correspondiente a "velocidad"
+        ninjacontroller2.moveSpeed = 6.5f;
+    }
+
+    if (habilidad == "cooldown")
+    {
+        image2.sprite = Resources.Load<Sprite>("Cooldown"); // Carga la imagen correspondiente a "cooldown"
+        bombucha.cooldownTime2 = .5f;
+    }
+}
+
 }
