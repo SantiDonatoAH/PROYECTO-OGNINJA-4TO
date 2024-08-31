06cbd97b31bd618e494729887ec1e164080c174f
Lautaro Urso <lautarourso@gmail.com>
2024-08-30 22:01:21 -0300

    Revert "miniavanze"
    This reverts commit aeee9d35923a1255434a9d3c0b08f7834abcd803.

--- Assets/VolumeSettings.cs
+++ Assets/VolumeSettings.cs
@@ -1,54 +1,44 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
+
 
 public class VolumeSettings : MonoBehaviour
 {
-    public Slider musicSlider;
-    public AudioSource audioSource;
-    private float volume = 1.0f;
 
-    private static VolumeSettings instance;
+    public  Slider musicSlider;
 
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);  // No destruir al cargar una nueva escena
-        }
-        else
-        {
-            Destroy(gameObject);  // Destruir esta instancia si ya existe otra
-            return;
-        }
-    }
+    public float volume = 0;
+    public AudioSource AudioSource;
 
     private void Start()
     {
-        // Cargar el volumen guardado
+        
         if (PlayerPrefs.HasKey("musicVolume"))
         {
-            volume = PlayerPrefs.GetFloat("musicVolume");
-            musicSlider.value = volume;
+            LoadVolume();
         }
         else
         {
-            volume = musicSlider.value;
+            SetMusicVolume();
         }
-
-        audioSource.volume = volume;
-
-        // Añadir listener para el slider
-        musicSlider.onValueChanged.AddListener(delegate { OnVolumeChange(); });
     }
 
-    public void OnVolumeChange()
+    private void Update()
     {
-        // Actualizar el volumen en base al slider
         volume = musicSlider.value;
-        audioSource.volume = volume;
-
-        // Guardar el valor del volumen
-        PlayerPrefs.SetFloat("musicVolume", volume);
+        AudioSource.volume = volume;
     }
+    public void SetMusicVolume(){
+       
+
+      
+    }
+
+    private void LoadVolume(){
+        AudioSource.volume=volume;
+
+        SetMusicVolume();
+    }
+
 }
