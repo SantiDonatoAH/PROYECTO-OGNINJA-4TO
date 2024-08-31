00180c85b2ce9c6fcec615cd91d30627412a55fc
bautistam1 <147621284+bautistam1@users.noreply.github.com>
2024-08-30 22:18:50 -0300

    PAUDPIWAAAAAAAAAAAAAA

--- Assets/abilitySelector.cs
+++ Assets/abilitySelector.cs
@@ -24,7 +24,6 @@ public class abilitySelector : MonoBehaviour
         Ninja1();
         Ninja2();
 
-        h1 = habilidades.habilidadesDropdown1.text;
     }
 
     // Update is called once per frame
