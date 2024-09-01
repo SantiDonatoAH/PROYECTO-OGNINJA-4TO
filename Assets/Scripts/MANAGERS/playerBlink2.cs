7da9ce965c09d51610dc0d2e4b7f36164f6ee9bc
Lautaro Urso <lautarourso@gmail.com>
2024-09-01 17:53:14 -0300

    asdasdas

--- Assets/Scripts/MANAGERS/playerBlink2.cs
+++ Assets/Scripts/MANAGERS/playerBlink2.cs
@@ -39,6 +39,7 @@ public class playerBlink2 : MonoBehaviour
             txt2.text = health.ToString();
             healthAmount = health; // Ajustar esta línea para que refleje correctamente la salud restante
             healthBar.fillAmount = healthAmount / total; // Cambia el divisor 
+            EnableBlink();
             Invoke("DisableBlink", 0.5f);
 
             if (health <= 0)
