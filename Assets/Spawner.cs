f33a0a1343b31a5fe855a13cc25595ec52b737cf
Lautaro Urso <73238286+t0y1@users.noreply.github.com>
2024-09-17 10:04:22 -0300

    cuaren

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -2,9 +2,16 @@ using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class Spawner : MonoBehaviour
 {
+
+    public Text txt1;
+    public Text txt2;
+
+    public int pts1;
+    public int pts2;
 
     public GameObject playerPrefab;
     public GameObject playerPrefab2;
@@ -27,6 +34,8 @@ public class Spawner : MonoBehaviour
     public Transform vidaT;
     void Start()
     {
+        pts2 = int.Parse(txt2.text);
+        pts1 = int.Parse(txt1.text);
         // Verifica si el jugador es el creador de la sala (primer jugador en la lista)
         if (PhotonNetwork.PlayerList.Length == 1)
         {
@@ -41,6 +50,13 @@ public class Spawner : MonoBehaviour
         {
             PhotonNetwork.Instantiate(playerPrefab2.name, ninja2.position, ninja2.rotation);
 
+            if (pts1 > 0 && pts2 > 0)
+            {
+                GameObject vidaInstanciada = PhotonNetwork.Instantiate(vida.name, vidaT.position, vidaT.rotation);
+                vidaInstanciada.transform.SetParent(GameObject.Find("Game UI").transform, false);
+
+                PhotonNetwork.Instantiate(playerPrefab.name, ninja1.position, ninja1.rotation);
+            }
             
         }
         else if (PhotonNetwork.PlayerList.Length == 0)
