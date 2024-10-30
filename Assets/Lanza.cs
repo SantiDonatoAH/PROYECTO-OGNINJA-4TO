ed20563b97d2bb23c801cb23853616e2090e40f3
Lautaro Urso <lautarourso@gmail.com>
2024-10-30 20:02:28 -0300

    Listo

--- Assets/Lanza.cs
+++ Assets/Lanza.cs
@@ -1,18 +1,151 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using Photon.Pun;
 
-public class Lanza : MonoBehaviour
+public class Lanza : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+
+    public Rigidbody2D rb;
+
+    public GameObject agua;
+    public float bulletSpeed = 2f;
+
+    public Animator anim;
+    public Animator anim2;
+    public GameObject ninja1;
+    public GameObject ninja2;
+
+    public int multiplicador = 0;
+    public int multiplicador2 = 0;
+
+    [SerializeField] private AudioClip pewSound;
+
+    public ScreenController pausemanager;
+
+    public bool canFire = true;  // Controla el cooldown para el primer jugador
+    public bool canFire2 = true; // Controla el cooldown para el segundo jugador
+    public float cooldownTime = 0.085f;
+    public float cooldownTime2 = 0.085f;
+
+    public float sumador = 0.02f;
+    public float sumador2 = 0.02f;
+
+    public float poder = 0f;
+    public float poder2 = 0f;
+
+    PhotonView view;
+    PhotonView view2;
     void Start()
     {
-        
+        ninja1 = GameObject.FindWithTag("player1");
+
+        ninja2 = GameObject.FindWithTag("player2");
+
+        anim = ninja1.GetComponent<Animator>();
+        anim2 = ninja2.GetComponent<Animator>();
+
+        view = ninja1.GetComponent<PhotonView>();
+        view2 = ninja2.GetComponent<PhotonView>();
     }
 
-    // Update is called once per frame
+    [PunRPC]
+    public void cd1(float cd)
+    {
+        sumador = cd;
+    }
+    [PunRPC]
+    public void cd2(float cd)
+    {
+        sumador2 = cd;
+    }
+
     void Update()
     {
-        
+
+        if (anim.GetBool("IsHoldingLanza") == true && Input.GetKey(KeyCode.LeftShift) && canFire && view.IsMine)
+        {
+            poder += sumador;
+            if (poder >= 8.1f)
+            {
+                poder = 8.1f;
+            }
+        }
+        else if (anim.GetBool("IsHoldingLanza") == true && Input.GetKeyUp(KeyCode.LeftShift) && view.IsMine)
+        {
+            Fire();
+        }
+
+        if (anim2.GetBool("IsHoldingLanza2") == true && Input.GetKey(KeyCode.L) && canFire2 && view2.IsMine)
+        {
+            poder2 += sumador2;
+            if (poder2 >= 8.1f)
+            {
+                poder2 = 8.1f;
+            }
+
+        }
+        else if (anim2.GetBool("IsHoldingLanza2") == true && Input.GetKeyUp(KeyCode.L) && view2.IsMine)
+        {
+            Fire2();
+        }
+
     }
+
+
+    void Fire()
+    {
+        Transform firePoint = ninja1.GetComponent<Transform>();
+
+        if (firePoint.rotation.y == 0)
+        {
+            multiplicador = 1; // Dirección normal hacia la derecha
+        }
+        else if (firePoint.rotation.y != 0) // Si el ninja está mirando hacia la izquierda
+        {
+            multiplicador = -1; // Cambia la dirección de disparo
+        }
+
+        GameObject nuevaBala = PhotonNetwork.Instantiate(agua.name, new Vector3(firePoint.position.x + (0.5f * multiplicador), firePoint.position.y, 0), firePoint.rotation);
+
+        rb = nuevaBala.GetComponent<Rigidbody2D>();
+        rb.velocity = firePoint.right * bulletSpeed * poder;
+    }
+
+   
+    void Fire2()
+    {
+
+        Transform firePoint = ninja2.GetComponent<Transform>();
+
+        if (firePoint.rotation.y == 0)
+        {
+            multiplicador = 1; // Dirección normal hacia la derecha
+        }
+        else if (firePoint.rotation.y != 0) // Si el ninja está mirando hacia la izquierda
+        {
+            multiplicador = -1; // Cambia la dirección de disparo
+        }
+
+        GameObject nuevaBala = PhotonNetwork.Instantiate(agua.name, new Vector3(firePoint.position.x + (0.5f * multiplicador), firePoint.position.y, 0), firePoint.rotation);
+
+        rb = nuevaBala.GetComponent<Rigidbody2D>();
+        rb.velocity = firePoint.right * bulletSpeed * poder2;
+    }
+
+
+ 
+
+    IEnumerator CooldownRoutine()
+    {
+        yield return new WaitForSeconds(cooldownTime);
+    }
+
+    IEnumerator CooldownRoutine2()
+    {
+        yield return new WaitForSeconds(cooldownTime2);
+
+    }
+
 }
